d34dc0fb9ef580ed3c639989e55a7c52416db017
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-17 16:22:38 +0500

    add to commit upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
