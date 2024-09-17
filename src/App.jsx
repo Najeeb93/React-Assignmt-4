94bf7568a0e93273e83f328e68dbf77bfc3c2b59
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-17 16:07:45 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div><h1>Hello World</h1></div>
+  )
+}
+
+export default App
