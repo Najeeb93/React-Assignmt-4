696d22644dfbe99a0a027a137ba85c212b9d2575
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-17 18:06:55 +0500

    upload

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -3,16 +3,27 @@ import { useEffect, useState } from "react";
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     axios
       .get("https://dummyjson.com/products")
-      .then((res) => console.log(res))
-      .catch((error) => console.log(error));
+      .then((res) => {
+        console.log(res.data.products);
+        setProducts(res.data.products);
+        setLoading(false)
+      })
+      
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+        
+      });
   }, []);
   return (
-    <div>
-      <h1>Products</h1>
+    <div className="container mx-auto">
+
+
     </div>
   );
 }
