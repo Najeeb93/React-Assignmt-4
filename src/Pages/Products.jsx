8210f4d1d8dff39f9d4adfd69f4a47d1e61ef827
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-17 17:14:38 +0500

    add to commit upload

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,9 +1,20 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Products() {
-    return (
-        <div><h1>Products</h1></div>
-    )
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <div>
+      <h1>Products</h1>
+    </div>
+  );
 }
 
 export default Products;
