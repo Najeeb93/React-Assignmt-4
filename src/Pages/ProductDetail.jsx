47fcd64cb9c5d8711f572475bb5c316b1212453a
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-18 20:04:52 +0500

    update changes

--- src/Pages/ProductDetail.jsx
+++ src/Pages/ProductDetail.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
+
 function ProdutDetail() {
   const { id } = useParams();
   const [product, setProduct] = useState({});
@@ -24,7 +25,7 @@ function ProdutDetail() {
   }, []);
 
   return (
-    <div className="container mx-auto">
+    <div className="container ">
       {loading ? (
         <h1 className="text-center text-3xl">Loading...</h1>
       ) : notFound ? (
@@ -35,10 +36,10 @@ function ProdutDetail() {
     <div className="lg:w-4/5 mx-auto flex flex-wrap">
       <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
         <h2 className="text-sm title-font text-gray-500 tracking-widest">
-          BRAND NAME
+          {product.category}
         </h2>
         <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
-          Animated Night Hill Illustrations
+         {product.title}
         </h1>
         <div className="flex mb-4">
           <a className="flex-grow text-indigo-500 border-b-2 border-indigo-500 py-2 text-lg px-1">
@@ -52,10 +53,7 @@ function ProdutDetail() {
           </a>
         </div>
         <p className="leading-relaxed mb-4">
-          Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-          sriracha taximy chia microdosing tilde DIY. XOXO fam inxigo juiceramps
-          cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine
-          tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean.
+            {product.description}
         </p>
         <div className="flex border-t border-gray-200 py-2">
           <span className="text-gray-500">Color</span>
@@ -71,10 +69,10 @@ function ProdutDetail() {
         </div>
         <div className="flex">
           <span className="title-font font-medium text-2xl text-gray-900">
-            $58.00
+            ${product.price}
           </span>
           <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-            Button
+            Add to Cart
           </button>
           <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
             <svg
@@ -93,15 +91,15 @@ function ProdutDetail() {
       <img
         alt="ecommerce"
         className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-        src="https://dummyimage.com/400x400"
+        src={product.thumbnail}
       />
     </div>
   </div>
 </section>
 
         
-    )}
-    </div>
+)}
+</div>
   );
 }
 
