b562c01fb16e6409ffb9de3817d3f22126ab1431
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-20 19:16:44 +0500

    update pagination

--- src/Pages/Pagination.jsx
+++ src/Pages/Pagination.jsx
@@ -2,6 +2,8 @@
 
 function Pagination() {
     return (
-        
+        <div>
+            <h1 className="font-medium text-3xl underline">Pagination in React Ecommerce </h1>
+        </div>
     )
 }
