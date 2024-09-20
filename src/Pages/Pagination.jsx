f7262bd01e82f4a8f0957442936fa9a882c0cc9a
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-21 00:30:19 +0500

    add to changes upload

--- src/Pages/Pagination.jsx
+++ src/Pages/Pagination.jsx
@@ -2,13 +2,20 @@ import { useEffect, useState } from "react";
 
 function Pagination() {
   const [products, setProducts] = useState([]);
-  const [limit, setLimit] = useState(10);
+  const [limit, setLimit] = useState(20);
   const [skip, setSkip] = useState(0);
+  const [total, setTotal] = useState(0);
   useEffect(() => {
-    fetch("https://dummyjson.com/products?limit=10&skip=10&select=title,price")
+    fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`)
       .then((res) => res.json())
-      .then(console.log);
+      .then((res)=> {
+        setProducts(res.products);
+        setTotal(res.total);
+      })
   }, []);
+  console.log('products-->', products);
+  console.log('total-->', total);
+  
   return (
     <div>
       <h1 className="font-medium text-3xl underline">
