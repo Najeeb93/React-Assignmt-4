b16c9f859466acac6643c344216526ab73a60b51
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-21 00:15:08 +0500

    add to commit upload

--- src/Pages/Pagination.jsx
+++ src/Pages/Pagination.jsx
@@ -1,6 +1,11 @@
+import { useEffect, useState } from "react";
 
 
 function Pagination() {
+    const [products, setProducts] = useState([])
+    useEffect(()=> {
+
+    }, [])
     return (
         <div>
             <h1 className="font-medium text-3xl underline">Pagination in React Ecommerce </h1>
