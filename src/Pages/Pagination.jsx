ebb1d34449e13d262f64243686303f5e609d8435
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-21 00:17:41 +0500

    update pagination

--- src/Pages/Pagination.jsx
+++ src/Pages/Pagination.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 
-
 function Pagination() {
-    const [products, setProducts] = useState([])
-    useEffect(()=> {
-
-    }, [])
-    return (
-        <div>
-            <h1 className="font-medium text-3xl underline">Pagination in React Ecommerce </h1>
-        </div>
-    )
-};
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://dummyjson.com/products?limit=10&skip=10&select=title,price")
+      .then((res) => res.json())
+      .then(console.log);
+  }, []);
+  return (
+    <div>
+      <h1 className="font-medium text-3xl underline">
+        Pagination in React Ecommerce
+      </h1>
+    </div>
+  );
+}
 
 export default Pagination;
