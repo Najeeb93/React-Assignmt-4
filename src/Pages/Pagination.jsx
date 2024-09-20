289baffb88ffddc18b1dbc711a520c402fa77a1c
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-21 00:19:35 +0500

    add to commit upload

--- src/Pages/Pagination.jsx
+++ src/Pages/Pagination.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 function Pagination() {
   const [products, setProducts] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [skip, setSkip] = useState(0);
   useEffect(() => {
     fetch("https://dummyjson.com/products?limit=10&skip=10&select=title,price")
       .then((res) => res.json())
