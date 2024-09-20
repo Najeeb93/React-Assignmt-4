13a87d0512c6ed4e3bc72bf104b6ce6b145afea9
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-20 17:17:30 +0500

    upload

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
         <header className="text-gray-600 body-font">
@@ -18,7 +20,8 @@ function Home() {
       <span className="ml-3 text-xl">Tailblocks</span>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-900">First Link</a>
+      <a className="mr-5 hover:text-gray-900"><Link to='/product'>Products</Link></a>
+
       <a className="mr-5 hover:text-gray-900">Second Link</a>
       <a className="mr-5 hover:text-gray-900">Third Link</a>
       <a className="mr-5 hover:text-gray-900">Fourth Link</a>
