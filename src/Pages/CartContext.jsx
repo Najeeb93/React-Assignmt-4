b34d9ee07701c461db9e39816147f63140d0530d
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-24 19:59:47 +0500

    add to commit

--- src/Pages/CartContext.jsx
+++ src/Pages/CartContext.jsx
@@ -1,30 +1,23 @@
 import { createContext, useState } from "react";
 
-
-
-
 export const Context = createContext();
 
-
 function CartContextProvider(children) {
-    const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
-    function addItemToCart(item) {
-        const arr = cartItems
+  function addItemToCart(item) {
+    const arr = cartItems;
 
-        const itemIndex = cartItems.findIndex((data)=> data.id == item.id)
-        if(itemIndex == -1)
-            {
-                arr.push({ ...item, quantity: 1});
-            }  else {
-                arr[itemIndex].quantity++;
-            }
+    const itemIndex = cartItems.findIndex((data) => data.id == item.id);
+    if (itemIndex == -1) {
+      arr.push({ ...item, quantity: 1 });
+    } else {
+      arr[itemIndex].quantity++;
     }
-    return (
-        <CartContextProvider.Provider>
-            {children}
-        </CartContextProvider.Provider>
-    )
+  }
+  return (
+    <CartContextProvider.Provider>{children}</CartContextProvider.Provider>
+  );
 }
 
 export default CartContextProvider;
