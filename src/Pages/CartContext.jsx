45f091f3a231a395889896f75b07380498742443
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-24 19:57:40 +0500

    add to cart context upload

--- src/Pages/CartContext.jsx
+++ src/Pages/CartContext.jsx
@@ -10,8 +10,15 @@ function CartContextProvider(children) {
     const [cartItems, setCartItems] = useState([]);
 
     function addItemToCart(item) {
+        const arr = cartItems
 
         const itemIndex = cartItems.findIndex((data)=> data.id == item.id)
+        if(itemIndex == -1)
+            {
+                arr.push({ ...item, quantity: 1});
+            }  else {
+                arr[itemIndex].quantity++;
+            }
     }
     return (
         <CartContextProvider.Provider>
