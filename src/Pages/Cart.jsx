f71628a17e227ad50ee471d493812a486a8e6e04
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-26 00:18:32 +0500

    add to commit cart file

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -9,13 +9,13 @@ function CartPage() {
     <div className="contianer mx-auto my-5">
       <h1 className="font-medium text-3xl underline">Cart Items</h1>
       {cartItems.map((data) => (
-        <div key={data.id} className="flex border my-2 p-3">
+        <div key={data.id} className="flex border items-center my-2 p-3">
           <Image src={data.thumbnail} height={100} width={150} />
           <div className="flex flex-col pl-5">
             <h1 className="font-medium text-xl mb-2">{data.title}</h1>
-            <h1 className="font-normal text-lg mb-2">{data.describtion}</h1>
+            <h1 className="font-normal text-lg mb-2">{data.description}</h1>
             <h1 className="font-normal text-lg mb-2">{data.category}</h1>
-            <h1 className="font-normal text-lg mb-2">Price {data.price}</h1>
+            <h1 className="font-normal text-lg mb-2">Price : {data.price}</h1>
 
           </div>
         </div>
