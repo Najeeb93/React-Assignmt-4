8027fc8e10fc36b850db3a9dac7f37698f362a4a
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-25 19:50:27 +0500

    add to commit cart

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,23 +1,21 @@
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
-
+import { Image } from "antd";
 
 function CartPage() {
-    const {cartItems} = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
 
-    return (
-        <div className="contianer mx-auto my-5">
-        <h1 className="font-medium text-3xl underline">
-            Cart Items
-        </h1>
-        {
-            cartItems.map((data)=> <div
-            className="flex border my-2 p-3"
-            ></div>)
-        }
+  return (
+    <div className="contianer mx-auto my-5">
+      <h1 className="font-medium text-3xl underline">Cart Items</h1>
+      {cartItems.map((data) => (
+        <div className="flex border my-2 p-3">
+          <Image src={data.thumbnail} height={60} width={60} />
+          
         </div>
-    )
+      ))}
+    </div>
+  );
 }
-
 
 export default CartPage;
