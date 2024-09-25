b4ea4d3c962e37a9e70267bf7a1e737a53610c79
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-25 19:47:02 +0500

    update cart file

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,11 +1,20 @@
+import { useContext } from "react";
+import { CartContext } from "./CartContext";
 
 
 function CartPage() {
+    const {cartItems} = useContext(CartContext);
+
     return (
         <div className="contianer mx-auto my-5">
         <h1 className="font-medium text-3xl underline">
             Cart Items
         </h1>
+        {
+            cartItems.map((data)=> <div
+            className="flex border my-2 p-3"
+            ></div>)
+        }
         </div>
     )
 }
